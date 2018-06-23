a283a2555dc575cfe331d5d7880ea1b90137f6ef
Daniel <dan_wong@outlook.com>
2018-06-23 17:14:42 +1200

    Markers displayed

--- src/App.js
+++ src/App.js
@@ -5,32 +5,63 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 import API from './api';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: [],
+    }
+  }
+
+  componentDidMount() {
+    let currentComponent = this;
+    API.getSearchTrack(1,1).then(function(response) {
+      const events = JSON.parse(response);
+      var markersList = [];
+
+      for (var i=0; i<events.events.length; i++) {
+        markersList.push(events.events[i]);
+      }
+
+      currentComponent.setState({
+        markers: markersList,
+      });
+    })
+  }
+
   render() {
-    const MyMapComponent = compose(
-      withProps({
-        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyC0DPzLiu9R47vA0XWr-GOyCr2cx8JBDaU&v=3.exp&libraries=geometry,drawing,places",
-        loadingElement: <div style={{ height: `100%` }} />,
-        containerElement: <div style={{ height: `100vh` }} />,
-        mapElement: <div style={{ height: `100%` }} />,
-      }),
-      withScriptjs,
-      withGoogleMap
-    )((props) =>
-      <GoogleMap
-        defaultZoom={16}
-        defaultCenter={{ lat: -36.852329, lng: 174.769116 }}
-      >
-        {props.isMarkerShown && <Marker position={{ lat: -36.852329, lng: 174.769116 }} />}
-      </GoogleMap>
-    )
+    if (this.state.markers && this.state.markers.length > 0) {
+      const MyMapComponent = compose(
+        withProps({
+          googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyC0DPzLiu9R47vA0XWr-GOyCr2cx8JBDaU&v=3.exp&libraries=geometry,drawing,places",
+          loadingElement: <div style={{ height: `100%` }} />,
+          containerElement: <div style={{ height: `100vh` }} />,
+          mapElement: <div style={{ height: `100%` }} />,
+        }),
+        withScriptjs,
+        withGoogleMap
+      )((props) =>
+        <GoogleMap
+          defaultZoom={16}
+          defaultCenter={{ lat: -36.852329, lng: 174.769116 }}
+        >
 
-    console.log(API.getSearchTrack(1, 1));
+        {
+          props.markers.map((marker => {
+            return <Marker position={{ lat: marker.lat, lng: marker.lng}} key={marker.id} />
+          }))
+        }
 
-    return (
-      <div>
-        <MyMapComponent isMarkerShown />
-      </div>
-    );
+        </GoogleMap>
+      )
+
+      return (
+        <div>
+          <MyMapComponent markers={this.state.markers}/>
+        </div>
+      );
+    } else {
+      return (<h1>Loading...</h1>)
+    }
   }
 }
 
--- src/api.js
+++ src/api.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 
-const headers = {
+var config = {
+  headers : {
     'x-amz-docs-region': 'ap-southeast-2',
-    'x-api-key': 'aChwigeT119iHNkwstc1satG5j2QToMQ8NPRKAzk'
-}
+    'x-api-key': '47mmRZ4JLK5Y7W0gQrhUs43WzU3YdNoq9wLvMNKG',
+  }  
+};
 
 export default {
     getSearchTrack(personID, groupID) {
-        return axios.get(`https://c44r10nquk.execute-api.ap-southeast-2.amazonaws.com/test/sarFunction?type=event&personAndGroupId=${personID}+${groupID}`, headers)
+        return axios.get(`https://4beodlhpjg.execute-api.us-east-1.amazonaws.com/dev/personandgroupid?personid=${personID}&groupid=${groupID}`, config)
             .then(response => {
-                return response;
+                return response.data.body;
             });
     }
 }
