4d276cd847e26a88d0acfafa6f0e40c670bdbd7e
Daniel <dan_wong@outlook.com>
2018-07-22 16:05:09 +1200

    Open-layers markers

--- src/components/map/SARMap.js
+++ src/components/map/SARMap.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Map, View, LonLat } from 'ol';
+import { Map, View, Feature } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
 import { transform } from 'ol/proj';
 import { Attribution, ScaleLine, defaults as DefaultControls } from 'ol/control';
-import OSM from 'ol/source/OSM';
+import SourceVector from 'ol/source/Vector';
+import LayerVector from 'ol/layer/Vector';
+import { Point } from 'ol/geom/';
+import { Style, Icon } from 'ol/style/';
 
 export default class SARMap extends React.Component {
   static propTypes = {
@@ -22,19 +25,44 @@ export default class SARMap extends React.Component {
   }
 
   componentDidMount() {
+    var vectorSource = new SourceVector({
+      //empty vector
+    });
+
+    var iconFeature = new Feature({
+      geometry: new Point(transform([174.7690, -36.8522], 'EPSG:4326',   'EPSG:3857')),
+      name: 'Auckland CBD'
+    });
+
+    vectorSource.addFeature(iconFeature);
+
+    //create the style
+    var iconStyle = new Style({
+      image: new Icon(/** @type {olx.style.IconOptions} */ ({
+        anchor: [0.5, 46],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'pixels',
+        opacity: 0.75,
+        src: '../../img/marker.png'
+      }))
+    });
+
+    var vectorLayer = new LayerVector({
+      source: vectorSource,
+      style: iconStyle
+    });
+
     let map = new Map({
       layers: [
         new TileLayer({
-          source: new OSM()
-        }),
-        new TileLayer({
           source: new XYZ({
-            url: 'http://tiles-{a-d}.data-cdn.linz.govt.nz/services;key=877beb090a4e4fab8c6ea96aefab3526/tiles/v4/layer=50768/EPSG:3857/{z}/{x}/{y}.png', //50767
+            url: 'http://tiles-{a-d}.data-cdn.linz.govt.nz/services;key=877beb090a4e4fab8c6ea96aefab3526/tiles/v4/layer=50767/EPSG:3857/{z}/{x}/{y}.png', //50767
             attributions: [
               new Attribution({ html: '<a href="http://data.linz.govt.nz">LINZ. CC BY 4.0</a>' })
             ]
           })
-        })
+        }),
+        vectorLayer
       ],
       target: 'map',
       view: new View({
@@ -49,7 +77,6 @@ export default class SARMap extends React.Component {
         new ScaleLine()
       ])
     });
-
   }
 
   render() {
