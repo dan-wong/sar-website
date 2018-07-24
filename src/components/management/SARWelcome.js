d6736243aea7467e7b1dd4c601b01d51aba10da7
victorlian <vlia679@aucklanduni.ac.nz>
2018-07-25 00:37:21 +1200

    added title, some extra fiddling

--- src/components/management/SARWelcome.js
+++ src/components/management/SARWelcome.js
@@ -36,35 +36,44 @@ const data = [
 function SARWelcome(props) {
   const { classes } = props;
 
+  const pStyle = {
+    fontSize: '45px',
+    textAlign: 'center',
+    color: 'darkBlue'
+  };
+
   return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell numeric >Calories</TableCell>
-            <TableCell numeric>Fat (g)</TableCell>
-            <TableCell numeric>Carbs (g)</TableCell>
-            <TableCell numeric>Protein (g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map(n => {
-            return (
-              <TableRow key={n.id}>
-                <TableCell component="a" href="google.com" scope="row">
-                  {n.name}
-                </TableCell>
-                <TableCell numeric >{n.calories}</TableCell>
-                <TableCell numeric>{n.fat}</TableCell>
-                <TableCell numeric>{n.carbs}</TableCell>
-                <TableCell numeric>{n.protein}</TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </Paper>
+    <div>
+      <h1 style={pStyle}>Search And Rescue App</h1>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell numeric >Calories</TableCell>
+              <TableCell numeric>Fat (g)</TableCell>
+              <TableCell numeric>Carbs (g)</TableCell>
+              <TableCell numeric>Protein (g)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map(n => {
+              return (
+                <TableRow key={n.id}>
+                  <TableCell component="a" href="google.com" scope="row">
+                    {n.name}
+                  </TableCell>
+                  <TableCell numeric >{n.calories}</TableCell>
+                  <TableCell numeric>{n.fat}</TableCell>
+                  <TableCell numeric>{n.carbs}</TableCell>
+                  <TableCell numeric>{n.protein}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
+    </div>
   );
 }
 
